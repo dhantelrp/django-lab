cff9f7bbd338740ab053ca8cc0b3feb599ad0cd8
Dhantel R. Prawedha <dhantel@stmik-aub.ac.id>
2021-11-06 16:17:01 +0700

    Penambahan Error Page

--- cms/blogs/views.py
+++ cms/blogs/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Blog
 
+
+def handler404(request):
+    return render(request, '404.html', status=404)
+
 def index(request):
     blogs = Blog.objects.all()
+    # versi 2 jika halaman ada tak ditemukan
+    # blogs = Blog.objects.all()
     # Versi 1. Menggunakan ini
     # output = ', '.join([str(blog) for blog in blogs])
     # return HttpResponse(output)
@@ -11,7 +17,8 @@ def index(request):
     return render(request, 'blogs/index.html', {'blogs': blogs})
 
 def single(request, id):
-    blog = Blog.objects.get(pk = id)
+    blog = get_object_or_404(Blog, pk=id)
+    # blog = Blog.objects.get(pk = id)
     return render(request, 'blogs/single.html', {'blog': blog})
 
 
--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cdet!p&f8gh9#%u@c(7*hg7fue@s$-=+8-@#n6*q=v!upe1uid'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 
 # Application definition
